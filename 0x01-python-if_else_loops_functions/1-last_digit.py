a6230a79d7db2b4c99e886f02dcaddb483db7708
YerardinPerlaza <yerardinpm@gmail.com>
2021-04-27 20:32:08 +0000

    correct negatives numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-lastdigit = number % 10
+lastdigit = abs(number) % 10
+print("Last digit of {} is {}".format(number, lastdigit), end="")
+
 if lastdigit > 5:
-    print('Last digit of', number, 'is', lastdigit, 'and is greater than 5')
+    print(' and is greater than 5')
 elif lastdigit == 0:
-    print('Last digit of', number, 'is', lastdigit, 'and is 0')
+    print(' and is 0')
 else:
-    print('Last digit of', number, 'is', lastdigit, 'and is less than 6 and not 0')
+    print(' and is less than 6 and not 0')
