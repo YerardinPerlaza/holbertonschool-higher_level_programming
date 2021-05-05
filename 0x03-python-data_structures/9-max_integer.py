6884a5ea3f9bae04d8bd82fd3529fd395027e00d
YerardinPerlaza <yerardinpm@gmail.com>
2021-05-05 22:31:20 +0000

    condition equal exercise 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,8 +2,8 @@
 def max_integer(my_list=[]):
     if not my_list:
         return (None)
-    maximum = 0
-    for i in my_list:
-        if maximum < my_list[i]:
+    maximum = my_list[0]
+    for i in range(len(my_list)):
+        if maximum <= my_list[i]:
             maximum = my_list[i]
     return (maximum)
