bad843414ccff8e919ea68483e80a699e4eff9fe
YerardinPerlaza <yerardinpm@gmail.com>
2021-05-05 22:01:08 +0000

    change el for

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,9 +2,8 @@
 def max_integer(my_list=[]):
     if not my_list:
         return (None)
-
     maximum = 0
-    for i in range(len(my_list)):
+    for i in my_list:
         if maximum < my_list[i]:
             maximum = my_list[i]
     return (maximum)
