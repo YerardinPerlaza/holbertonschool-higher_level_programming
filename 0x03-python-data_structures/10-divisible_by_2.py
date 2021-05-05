f5a0caa93e5ac56c9911a931ec014bfc1f0ddaf5
YerardinPerlaza <yerardinpm@gmail.com>
2021-05-05 22:51:52 +0000

    no create another list

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    new_list = list(my_list)
-    for i in new_list:
+    for i in my_list:
         if i % 2 == 0:
-            new_list[i] = True
+            my_list[i] = True
         else:
-            new_list[i] = False
-    return new_list
+            my_list[i] = False
+    return my_list
