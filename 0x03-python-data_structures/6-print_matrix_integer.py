59a6a82fefaa0aa535be99258d3181c21217ee04
YerardinPerlaza <yerardinpm@gmail.com>
2021-05-05 20:46:38 +0000

    correct whitespace pep8

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    for i in range (len(matrix)):
-        for j in range (len(matrix[i])):
-                if j != len(matrix[i]) - 1:
-                    space = ' '
-                else:
-                    space = ''
-                print("{:d}".format(matrix[i][j]), end=space)
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if j != len(matrix[i]) - 1:
+                space = ' '
+            else:
+                space = ''
+            print("{:d}".format(matrix[i][j]), end=space)
         print()
