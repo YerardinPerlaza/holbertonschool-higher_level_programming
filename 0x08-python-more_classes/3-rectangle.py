19c4a6b6ca9c3265e547b7f9ffa705aed83186ed
YerardinPerlaza <yerardinpm@gmail.com>
2021-05-20 23:35:50 +0000

    fix pep8

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """class rectangle"""
 
+
 class Rectangle:
     """initialize rectangle"""
     def __init__(self, width=0, height=0):
@@ -58,4 +59,3 @@ class Rectangle:
             rectangle += "#" * self.__width + '\n'
         rectangle += "#" * self.__width
         return rectangle
-
--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """class rectangle"""
 
+
 class Rectangle:
     """initialize rectangle"""
 
