ad7fffd5d241e128006acdaee119ac75092d92d0
YerardinPerlaza <yerardinpm@gmail.com>
2021-06-02 23:18:15 +0000

    fix 0 without new line

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """read a file with WITH"""
 
     with open(filename, encoding="utf-8") as myfile:
-        print(myfile.read())
+        print(myfile.read(), end="")
