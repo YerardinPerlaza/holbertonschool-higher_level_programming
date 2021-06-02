d69311dd9ebe4b72fa0c54d366ce1bf45d93090f
YerardinPerlaza <yerardinpm@gmail.com>
2021-06-02 01:11:56 +0000

    fix documentation 0-1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-'''
-Write a class MyList that inherits from list
-'''
+''' Write a class MyList that inherits from list '''
 
 
 class MyList(list):
-    '''
-    prints the list, but sorted
-    '''
+    ''' prints the list, but sorted '''
     def print_sorted(self):
+        ''' prints the list, but sorted '''
         print(sorted(self))
