291667ed0319b81aa7a637f4ae18b4ba8731039e
YerardinPerlaza <yerardinpm@gmail.com>
2021-07-14 01:01:54 +0000

    correct task 5

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,3 +1,2 @@
 --script that creates the table unique_id
--- unique_id description
-CREATE TABLE IF NOT EXISTS unique_id(id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
