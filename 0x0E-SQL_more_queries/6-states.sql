1bfa78c7b0f28a087dce67e9e6dcc69093f647cd
YerardinPerlaza <yerardinpm@gmail.com>
2021-07-14 00:59:03 +0000

    check comments 6, 7 ,8

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,5 @@
 --creates the database hbtn_0d_usa and the table states
+--states description
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, name VARCHAR(256) NOT NULL);
