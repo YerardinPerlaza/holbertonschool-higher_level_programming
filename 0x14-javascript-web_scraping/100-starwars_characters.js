4ccbf713caf9b15b03802f2b24e95baa696eee62
YerardinPerlaza <yerardinpm@gmail.com>
2021-09-15 01:38:09 +0000

    solve task 6 and advance 100

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+require('request').get('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, resp, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    const characters = JSON.parse(body).characters;
+    for (const character of characters) {
+      require('request')(character, function (error, resp, body) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
+  }
+});
